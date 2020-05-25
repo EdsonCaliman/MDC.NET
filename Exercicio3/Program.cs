a3155e6e153814083ea6cfe3fb3a5eb2b5f9161b
Edson Caliman <edsonjunior@db1.com.br>
2020-05-25 09:25:24 -0300

    Console Exercicio3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Múltiplos de 3, entre 1 e 100:");
+            var contador = 1;
+            int resto;
+            while (contador <= 100)
+            {
+                //Math.DivRem(contador, 3, out resto);
+                resto = contador % 3;
+                if (resto == 0) 
+                {
+                    Console.WriteLine(contador);
+                }
+                contador++;
+            }
+        }
+    }
+}
