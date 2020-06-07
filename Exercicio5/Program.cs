99373832cddd98cef86d7dbb4c65508c2a968d38
Edson Caliman <edsonjunior@db1.com.br>
2020-06-07 11:31:52 -0300

    Console Exercicio6

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcularFormulaDeBhaskara();
+        }
+
+        private static void CalcularFormulaDeBhaskara()
+        {
+            Console.WriteLine("Calculo da fórmula de Bhaskara");
+            Console.WriteLine("Digite o valor a:");
+            var valorA = Console.ReadLine();
+            Console.WriteLine("Digite o valor b:");
+            var valorB = Console.ReadLine();
+            Console.WriteLine("Digite o valor c:");
+            var valorC = Console.ReadLine();
+
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            try
+            {
+                a = Convert.ToDouble(valorA);
+                b = Convert.ToDouble(valorB);
+                c = Convert.ToDouble(valorC);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Os valores informados devem ser números");
+                Console.ReadLine();
+                CalcularFormulaDeBhaskara();
+                return;
+            }
+
+            double delta = Math.Pow(b, 2) - (4 * a * c);
+            double raiz = Math.Sqrt(delta);
+            var r1 = ((-(b)) + raiz) / (2 * a);
+            var r2 = ((-(b)) - raiz) / (2 * a);
+
+            Console.WriteLine("Valor de R1: {0}", r1);
+            Console.WriteLine("Valor de R2: {0}", r2);
+        }
+    }
+}
---
+++ Exercicio6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorOut = 20;
+            int valorRef = 20;
+            int valor = 20;
+            Console.WriteLine("Valor SomarRef:" + SomarRef(ref valorOut)); //não altera o valor
+            Console.WriteLine("Valor SomarOut:" + SomarOut(out valorRef)); //altera o valor, mas deve ser inicializado, exclusivo para retorno
+            Console.WriteLine("Valor Somar:" + Somar(valor)); //default ref
+        }
+
+        private static int Somar(int valor)
+        {
+            return valor + valor;
+        }
+
+        private static int SomarOut(out int valorRef)
+        {
+            valorRef = 0;
+            return valorRef + valorRef;
+        }
+
+        private static int SomarRef(ref int valorOut)
+        {
+            return valorOut + valorOut;
+        }
+    }
+}
