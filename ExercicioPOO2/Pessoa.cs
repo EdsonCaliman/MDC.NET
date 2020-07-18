ca11d44529de52eab5a52bd5759356481e8ec09d
Edson Caliman <edsonjunior@db1.com.br>
2020-07-18 09:44:11 -0300

    Console ExercicioPOO2

---
+++ ExercicioPOO2/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExercicioPOO2
+{
+    class Pessoa
+    {
+        private string _nome;
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value;
+        }
+
+        private DateTime _dataNascimento;
+
+        public DateTime DataNascimento
+        {
+            get => _dataNascimento;
+            set => _dataNascimento = value;
+        }
+
+        private double _altura;
+        public double Altura
+        {
+            get => _altura;
+            set => _altura = value;
+        }
+
+        public Pessoa(string nome, DateTime datanascimento, double altura)
+        {
+            _nome = nome;
+            _dataNascimento = datanascimento;
+            _altura = altura;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + _nome + " Data Nascimento: " + (_dataNascimento.ToString("dd/MM/yyyy")) + " Altura: " + _altura + " Idade: " + this.RetornarIdadedaPessoa();
+        }
+
+        public int RetornarIdadedaPessoa()
+        {
+            return DateTime.Today.Year - _dataNascimento.Year;
+        }
+    }
+}
---
+++ ExercicioPOO2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioPOO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o Nome da Pessoa");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe a Data de Nascimento DD/MM/YYYY");
+            string datanascimento = Console.ReadLine();
+            Console.WriteLine("Informe a altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(nome, Convert.ToDateTime(datanascimento), altura);
+
+            Console.WriteLine(pessoa.ToString());
+
+        }
+    }
+}
